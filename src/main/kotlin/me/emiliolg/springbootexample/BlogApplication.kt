8237585eb7c68dd82f087085b7df234701e6d5fb
Emilio Lopez Gabeiras <emiliolg@gmail.com>
2020-11-26 17:31:39 -0300

    Add Project

---
+++ src/main/kotlin/me/emiliolg/springbootexample/BlogApplication.kt
@@ -0,0 +1,15 @@
+package me.emiliolg.springbootexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
+class BlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogApplication>(*args) {
+        //setBannerMode(Banner.Mode.OFF)
+    }
+}
